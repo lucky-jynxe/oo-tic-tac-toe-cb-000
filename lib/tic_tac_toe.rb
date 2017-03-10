a5629e6c6105f9820aaa07eb2ffcf9a6ef449a49
Kayla Gutierrez <hellokayla88@gmail.com>
2017-03-10 07:06:05 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,13 +86,7 @@ class TicTacToe
   end
 
   def play
-    until over?
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cat's Game!"
-    end
+    turn until over?
+    winner ? (puts "Congratulations #{winner}!") : (puts "Cat's Game!")
   end
 end
